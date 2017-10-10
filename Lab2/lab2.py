8f640ae31612df269ae30b43e5eaa68a543fdd39
robertbutacu <robert.butacu@yahoo.com>
2017-10-10 12:59:22 +0300

    ex 3

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,5 +1,7 @@
 import re
 
+import itertools
+
 
 def is_prime(input):
     return all(map(lambda x: input % x != 0, list(range(2, input // 2))))
@@ -32,3 +34,21 @@ def list_ops(a, b):
 
 
 print("2. " + str(list_ops([1, 2, 3, 4, 5, 6, 7], [2, 3, 4, 5, 6, 7, 8, 8])))
+
+'''
+Sa se scrie o functie care primeste ca parametru un numar variabil de liste si un numar intreg x. 
+Sa se returneze o lista care sa contina elementele care apar de exact x ori in listele primite. 
+Exemplu: pentru listele [1,2,3], [2,3,4], [4,5,6], [4, 1, "test"] si x = 2 
+se va returna [1, 2, 3, 4] # 1 se afla in lista 1 si 4, 2 se afla in lista 1 si 2, 3 se afla in listele 1 si 2, 4 se afla in listele 2 si 3.
+'''
+
+
+def counter(lists, app):
+    merged = list(itertools.chain(*lists))
+    different_items = set(merged)
+
+    apparitions = list(map(lambda x: merged.count(x), different_items))
+    return list(filter(lambda x: x[1] == app, zip(different_items, apparitions)))
+
+
+print("3." + str(counter([[1, 2, 3], [2, 3, 4], [4, 5, 6], [4, 1, "test"]], 2)))
