792dda870c1e152bd4d18bc45a6ed0809c88cd27
robertbutacu <robert.butacu@yahoo.com>
2017-10-20 23:18:32 +0300

    ex 2

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -2,6 +2,7 @@
     si returneaza un tuplu de seturi care sa contina: (a intersectat cu b, a reunit cu b, a - b, b - a)
 '''
 
+
 def ex1(a, b):
     intersection = map(lambda x: x in b, a)
     reunion = set(a + b)
@@ -10,3 +11,27 @@ def ex1(a, b):
 
     return intersection, reunion, a_minus_b, b_minus_a
 
+
+'''2. Scrieti o functie care primeste ca parametru un sir de caractere si 
+returneaza un dictionar in care cheile sunt caracterele dn componenta sirului de caractere 
+iar valorile sunt reprezentate de numarul de aparitii ale caracterului respectiv in textul dat.
+Exemplu: Pentru sirul "Ana are mere." dat ca parametru functia va returna dictionarul:
+ {'A': 1, ' ': 2, 'n': 1, 'a': 2, 'r': 2, 'e': 3, 'm': 1, '.': 1}.'''
+
+
+def ex2(i):
+    def go(l, d):
+        if len(l) == 0:
+            return d
+
+        if l[0] in d.keys():
+            d[l[0]] = d[l[0]] + 1
+        else:
+            d[l[0]] = 1
+
+        return go(l[1:], d)
+
+    return go(i, {})
+
+
+print(ex2("Ana are mere"))
