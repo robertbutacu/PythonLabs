7dcfe0443ae94c565c6dce0b7356c896ffcc0b64
robertbutacu <robert.butacu@yahoo.com>
2017-11-18 14:07:41 +0200

    ex2

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -5,6 +5,7 @@ b) a+b
 c) a/b
 d) a*b
 '''
+import os
 
 
 def ops(a, b):
@@ -20,3 +21,31 @@ def ops(a, b):
 
 
 ops(3, 0)
+
+
+'''
+2. Scrieti un script care primeste ca parametru de la linia de comanda un path si
+afiseaza primii 4096 bytes daca path-ul duce la un fisier, 
+intrarile din acesta daca path-ul reprezinta un director 
+si un mesaj de eroare daca path-ul nu este unul valid.'''
+
+
+def printInfo(path):
+    try:
+        if not os.path.exists(path):
+            raise IOError("Invalid path")
+
+        if os.path.isdir(path):
+            print("dir")
+            print(os.listdir(path))
+        elif os.path.isfile(path):
+            with open(path) as file:
+                print(file.read(4096))
+    except IOError as e:
+        print(e)
+        print("Provide a valid file/dir!")
+    except Exception as e:
+        print(e)
+
+
+printInfo("E:\\Downloadsad")
